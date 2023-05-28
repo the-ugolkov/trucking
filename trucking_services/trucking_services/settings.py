3c77af207dbfc7a60ffaabd1aae7456692535e8f
Mikhail <113590874+the-ugolkov@users.noreply.github.com>
2023-05-29 00:17:03 +0700

    настроил сборку контейнеров для работы периодической задачи и сборки всех представлений для них, так же исправил настройки для редис и celery в контексте докера

--- trucking_services/trucking_services/settings.py
+++ trucking_services/trucking_services/settings.py
@@ -136,8 +136,8 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
